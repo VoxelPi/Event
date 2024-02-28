700942b154dbf4fea10172d6640696358d5f9add
Peter Smek <smek.peter@gmail.com>
2024-02-28 02:36:40 +0100

    Change event priorities to constants for annotations

--- src/main/kotlin/net/voxelpi/event/EventPriorities.kt
+++ src/main/kotlin/net/voxelpi/event/EventPriorities.kt
@@ -2,13 +2,13 @@ package net.voxelpi.event
 
 object EventPriorities {
 
-    val EARLIEST = 1000
+    const val EARLIEST = 1000
 
-    val EARLY = 100
+    const val EARLY = 100
 
-    val NORMAL = 0
+    const val NORMAL = 0
 
-    val LATE = -100
+    const val LATE = -100
 
-    val LATEST = -1000
+    const val LATEST = -1000
 }
