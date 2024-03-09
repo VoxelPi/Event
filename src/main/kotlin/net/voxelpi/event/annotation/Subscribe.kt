ff0948644f3eaa9c953db003887f1c8fbde4c402
Peter Smek <smek.peter@gmail.com>
2024-03-09 01:59:57 +0100

    Add documentation for Subscribe annotation

--- src/main/kotlin/net/voxelpi/event/annotation/Subscribe.kt
+++ src/main/kotlin/net/voxelpi/event/annotation/Subscribe.kt
@@ -1,4 +1,8 @@
 package net.voxelpi.event.annotation
 
+/**
+ * Annotates a function to be used as an event subscriber.
+ * The function must have exactly one parameter that is used as the event type and must return [Unit].
+ */
 @Target(AnnotationTarget.FUNCTION)
 public annotation class Subscribe(val priority: Int = 0)
